d9ac94a3a3f03f0867ab0e07e35228de9914ec20
Alex Frania <aafrania@alaska.edu>
2019-08-30 10:13:32 -0800

    New Lab Project

---
+++ Labs/Labs 1 Project/Labs 1 Project/main.cpp
@@ -0,0 +1,19 @@
+//Program: Lab 1
+//Author: Alex Frania
+//Date: 8/30/19
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	
+	int a = 24;
+	int b = 2;
+	int c = a * b;
+
+	std::cout << a << "*" << b << "=" << c << std::endl;
+	 ff
+
+	return 0;
+}
