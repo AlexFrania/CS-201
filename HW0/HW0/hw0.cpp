cc93fe34fc2b781b9de76e9010b86b9c22264f61
Alex Frania <alexfrania96@gmail.com>
2019-09-04 01:52:30 -0800

    Added function poem() and gameDescript(). Made up of simple string expressions

--- HW0/HW0/hw0.cpp
+++ HW0/HW0/hw0.cpp
@@ -10,10 +10,31 @@ string repeat(string m, int n)
 	return m;
 }
 
+void poem()
+{
+	cout << "" << endl;
+	cout << "O God! Can I not grasp" << endl;
+	cout << "Them with a tighter clasp?" << endl;
+	cout << "O God! Can I not save" << endl;
+	cout << "One from the pitiless wave?" << endl;
+	cout << "Is all that we see or seem" << endl;
+	cout << "But a dream within a dream?" << endl;
+	cout << "" << endl;
+}
+
+void gameDescript()
+{
+	cout << "Resident Evil 2 is a survival horror game developed and published by Capcom." << endl;
+	cout << "Players control police officer Leon S. Kennedy and college student Claire Redfield" << endl;
+	cout << "as they attempt to escape from Raccoon City during a zombie apocalypse." << endl;
+}
+
 int main()
 {
 	string m = "|Hello, World| ";
 	int n = 5;
 	cout << repeat(m, n) << endl;
+	poem();
+	gameDescript();
 	return 0;
 }
