08d6dd5b6a2e269f78aa83da7293c3375b8e04ac
Alex Frania <alexfrania96@gmail.com>
2019-09-09 09:45:48 -0800

    Made SecondGrid(). Made note header.

--- Labs/Lab 3/Lab3.cpp
+++ Labs/Lab 3/Lab3.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 
+// Alex Frania
+// CS-201
+// Lab 3
+
 using std::cout;
 using std::endl;
 
 int a = 1;
 int b = 0;
+int c = 0;
 
+int SecondGrid()
+{
+	while (c < 600)
+	{
+		++c;
+		if (c % 2 != 0)
+		{
+			cout << "#";
+		}
+		else
+		{
+			cout << "@";
+		}
+		if (c % 60 == 0)
+		{
+			cout << "\n";
+		}
+	}
+	return 0;
+}
 
 int FirstGrid()
 {
 	while (b < 600)
 	{
-		cout << "#";
 		++b;
+		cout << "#";
 		if (b % 60 == 0)
 		{
 			cout << "\n";
 		}
 	}
+	cout << "\n";
+	SecondGrid();
 	return 0;
 }
 
@@ -28,6 +55,7 @@ int main()
 		cout << a << endl;
 		++a;
 	}
+	cout << "\n";
 	FirstGrid();
 	return 0;
 }
