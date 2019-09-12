c495e56350ddf5d65e797aa6ff1112d0263bac6e
Alex Frania <alexfrania96@gmail.com>
2019-09-11 18:18:44 -0800

    Modified for loop to contain extra else if statements to fit in "Buzz" and "Fizz Buzz" variables.

--- Labs/Lab 4/Source.cpp
+++ Labs/Lab 4/Source.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 
+//Alex Frania
+//CS-201
+//Lab 4
+
 using std::cout;
 
 int main()
 {
 	for (int a = 1; a <= 100; ++a)
 	{
-		if (a % 3 == 0)
+		if (a % 3 == 0 && a % 5 == 0)
+		{
+			cout << "Fizz Buzz\n";
+		}
+		else if (a % 3 == 0)
 		{
 			cout << "Fizz\n";
+		}
+		else if (a % 5 == 0)
+		{
+			cout << "Buzz\n";
 		}
 		else
 		{
