86f55ed82a403bf667e61b731a43625e901a7813
Alex Frania <alexfrania96@gmail.com>
2019-09-11 17:44:15 -0800

    Revert "Lab 3 final."
    This reverts commit 5a73186b312cb9ce4e6fc4de537558902ce1eaa4.

--- Labs/Lab 3/Lab3.cpp
+++ Labs/Lab 3/Lab3.cpp
@@ -1,62 +1,26 @@
 #include <iostream>
 
-// Alex Frania
-// CS-201
-// Lab 3
-
 using std::cout;
 using std::endl;
 
-int SecondGrid()
-{
-	int row = 0;
-	int c = 0;
-	while (c < 600)
-	{
-		++c;
-		if ((c + row) % 2 != 0)
-		{
-			cout << "#";
-		}
-		else
-		{
-			cout << "@";
-		}
-		if (c % 60 == 0)
-		{
-			row = 1 - row;
-			cout << "\n";
-		}
-	}
-	return 0;
-}
+int a = 1;
 
 int FirstGrid()
 {
-	int b = 0;
-	while (b < 600)
+	while (a <= 30)
 	{
-		++b;
 		cout << "#";
-		if (b % 60 == 0)
-		{
-			cout << "\n";
-		}
+		a++;
 	}
-	cout << "\n";
-	SecondGrid();
-	return 0;
 }
 
 int main()
 {
-	int a = 1;
 	while (a <= 20)
 	{
 		cout << a << endl;
-		++a;
+		a++;
 	}
-	cout << "\n";
 	FirstGrid();
 	return 0;
 }
