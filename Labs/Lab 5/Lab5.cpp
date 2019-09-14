7bc3be350ab4b80508f2a40273c52a888289b15c
Alex Frania <alexfrania96@gmail.com>
2019-09-13 16:58:25 -0800

    Added a if-statement functions that runs exactly like the switch-statement.

--- Labs/Lab 5/Lab5.cpp
+++ Labs/Lab 5/Lab5.cpp
@@ -3,6 +3,27 @@
 using std::cin;
 using std::cout;
 
+int ifVersion()
+{
+	cout << "Please enter an integer: ";
+	int i;
+	cin >> i;
+	if (i % 3 == 0)
+	{
+		cout << "Number is a multiple of 3.";
+	}
+	else if (i % 3 == 1)
+	{
+		cout << i << "+2 gives you a multiple of 3.";
+	}
+	else if (i % 3 == 2)
+	{
+		cout << i << "+1 gives you a multiple of 3.";
+	}
+	cout << "\n";
+	return 0;
+}
+
 int main()
 {
 	cout << "Please enter an integer: ";
@@ -21,5 +42,8 @@ int main()
 		break;
 	}
 	cout << "\n";
+	cout << "\n";
+	cout << "Here's the if-statement version of the program:\n";
+	ifVersion();
 	return 0;
 }
