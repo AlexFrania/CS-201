c213baa83cc43bde56f55103bce26504c312bb94
Alex Frania <alexfrania96@gmail.com>
2019-09-15 13:59:05 -0800

    Aligned column labels and made increasing row number within loop.

--- HW/HW1/grid.cpp
+++ HW/HW1/grid.cpp
@@ -5,20 +5,20 @@ using std::cin;
 
 int main()
 {
-	for (int column = 1; column <= 16; column++)
+	for (int column = 1; column <= 15; column++)
 	{
 		cout << " ";
-		while (column == 4)
+		while (column == 3)
 		{
 			cout << "A";
 			break;
 		}
-		while (column == 6)
+		while (column == 7)
 		{
 			cout << "B";
 			break;
 		}
-		while (column == 10)
+		while (column == 11)
 		{
 			cout << "C";
 			break;
@@ -26,28 +26,28 @@ int main()
 	}
 	cout << "\n";
 	cout << " ";
+	int rownumb = 1;
 	for (int div = 1; div <= 60; div++) //row dividers
 	{
 		cout << "-";
 		while (div % 15 == 0 && div < 60) //breaks line at 15th period and ends on 60th
 		{
 			cout << "\n";
-			for (int rownumb = 1; rownumb <= 3; rownumb++)
+			cout << rownumb;
+			rownumb++;
+			for (int n = 1; n <= 3; n++) //content of rows, split into 3 sections
 			{
-				cout << rownumb;
-				for (int n = 1; n <= 3; n++) //content of rows, split into 3 sections
+				cout << "|";
+				cout << " ";
+				for (int point = 1; point <= 1; point++) //work on this later, core of game
 				{
-					cout << "|";
-					cout << " ";
-					for (int space = 1; space <= 1; space++) //work on this later, core of game
-					{
-						cout << "x";
-					}
-					cout << " ";
-					cout << "|";
+					cout << ".";
 				}
-				cout << "\n";
+				cout << " ";
+				cout << "|";
 			}
+			cout << "\n";
+			cout << " ";
 			break;
 		}
 	}
