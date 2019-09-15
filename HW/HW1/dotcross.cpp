048bd1efe557d711250e16f59077538905e1f0cb
Alex Frania <alexfrania96@gmail.com>
2019-09-15 13:37:20 -0800

    Made column labels (not aligned) and working on making row number loop.

--- HW/HW1/dotcross.cpp
+++ HW/HW1/dotcross.cpp
@@ -8,7 +8,7 @@ int dotcross()
 {
 	float x, y, z = 0;
 	float x2, y2, z2 = 0;
-	cout << "Enter in three floating-point integers for vector A.\n";
+	cout << "Enter in three floating-point numbers for vector A.\n";
 	cout << "x: ";
 	cin >> x;
 	cout << "y: ";
@@ -22,10 +22,10 @@ int dotcross()
 	cin >> y2;
 	cout << "z: ";
 	cin >> z2;
-	cout << '\n';
+	cout << "\n";
 	float dot = (x * x2) + (y * y2) + (z * z2);
 	cout << "A dot B = " << std::fixed << std::setprecision(5) << dot;
-	cout << '\n';
+	cout << "\n";
 	float Cx = (y * z2) - (z * y2);
 	float Cy = (z * x2) - (x * z2);
 	float Cz = (x * y2) - (y * x2);
--- HW/HW1/grid.cpp
+++ HW/HW1/grid.cpp
@@ -5,22 +5,49 @@ using std::cin;
 
 int main()
 {
+	for (int column = 1; column <= 16; column++)
+	{
+		cout << " ";
+		while (column == 4)
+		{
+			cout << "A";
+			break;
+		}
+		while (column == 6)
+		{
+			cout << "B";
+			break;
+		}
+		while (column == 10)
+		{
+			cout << "C";
+			break;
+		}
+	}
+	cout << "\n";
+	cout << " ";
 	for (int div = 1; div <= 60; div++) //row dividers
 	{
 		cout << "-";
 		while (div % 15 == 0 && div < 60) //breaks line at 15th period and ends on 60th
 		{
 			cout << "\n";
-			for (int n = 1; n <= 3; n++) //content of rows, split into 3 sections
+			for (int rownumb = 1; rownumb <= 3; rownumb++)
 			{
-				cout << "|";
-				for (int space = 1; space <= 3; space++)
+				cout << rownumb;
+				for (int n = 1; n <= 3; n++) //content of rows, split into 3 sections
 				{
+					cout << "|";
 					cout << " ";
+					for (int space = 1; space <= 1; space++) //work on this later, core of game
+					{
+						cout << "x";
+					}
+					cout << " ";
+					cout << "|";
 				}
-				cout << "|";
+				cout << "\n";
 			}
-			cout << "\n";
 			break;
 		}
 	}
