62ddf74c1d4a322e89e1cef004bc5e0a245d79fe
Alex Frania <alexfrania96@gmail.com>
2019-09-16 22:31:11 -0800

    Finished Lab 6.

---
+++ Labs/Lab 6/Lab6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void WordStuff(std::string word)
+{
+	using std::cout;
+	int len = word.size();
+	cout << "Input "<< '"' << word << '"' << " is " << len << " characters long.\n";
+	cout << "\n";
+	for (int n = 1; n <= len; n++)
+	{
+		cout << "#";
+	}
+	cout << "\n";
+	for (int i = (len - 1); i != -1; i--)
+	{
+		cout << word[i];
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	std::string input;
+
+	std::cout << "Type in a word: ";
+	std::cin >> input;
+	WordStuff(input);
+	return 0;
+}
