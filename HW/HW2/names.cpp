4c8eff132f2e0cc27ddd5d6b86c5ba020dee2961
Alex Frania <alexfrania96@gmail.com>
2019-09-18 10:15:26 -0800

    Need to work through Lab 7.

---
+++ HW/HW2/names.cpp
@@ -0,0 +1,21 @@
+/*Alex Frania
+CS-201
+Date: 9/18/19
+Description: 
+*/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+int main(int argc, char **argv)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		std::string name;
+		std::cout << "Please enter a name: ";
+		std::getline(cin, name);
+		
+	}
+	return 0;
+}
---
+++ Labs/Lab 7/name.cpp
@@ -0,0 +1,18 @@
+/*Alex Frania
+CS-201
+Lab 7*/
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int i1;
+	int i2;
+	int i3;
+	std::cin >> i1;
+	std::cin >> i2;
+	std::cin >> i3;
+
+	return 0;
+}
