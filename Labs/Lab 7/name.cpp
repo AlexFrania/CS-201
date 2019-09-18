11ce781f55dae42eb5278e5b430dc5848d69ce74
Alex Frania <alexfrania96@gmail.com>
2019-09-18 10:19:36 -0800

    Try again.

--- Labs/Lab 7/name.cpp
+++ Labs/Lab 7/name.cpp
@@ -4,15 +4,13 @@ Lab 7*/
 
 #include <iostream>
 #include <string>
+#include "intio.hpp"
 
 int main()
 {
 	int i1;
-	int i2;
-	int i3;
 	std::cin >> i1;
-	std::cin >> i2;
-	std::cin >> i3;
+	i1 = getInt();
 
 	return 0;
 }
