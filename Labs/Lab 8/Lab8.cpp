7e31f2897467b65a526b66337af9cf6a1838e50b
Alex Frania <alexfrania96@gmail.com>
2019-09-20 09:50:49 -0800

    Downloaded and implemented exterrnal files to Lab 8.

---
+++ Labs/Lab 8/Lab8.cpp
@@ -0,0 +1,20 @@
+/*Alex Frania
+CS-201
+Date: 9/20/19
+Lab 8*/
+
+#include <iostream>
+#include "lab8.hpp"
+#include "intio.hpp"
+
+int main()
+{
+	int n;
+	std::cin >> n;
+	if (n <= 0)
+	{
+		
+	}
+
+	return 0;
+}
