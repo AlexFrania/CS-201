ff633d975997ddbe8f6111710dafb0e46e1be99f
alexfrania96 <alexfrania96@gmail.com>
2019-09-20 13:54:36 -0800

    Finished additional for Lab 8 and am working. Working on HW2.

--- Labs/Lab 8/Lab8.cpp
+++ Labs/Lab 8/Lab8.cpp
@@ -9,22 +9,17 @@ int doInput()
 
 int compute(int i)
 {
-	return i * i;
-}
-
-
-int compute(int i)
-{
 	if (i <= 0)
 	{
 		return 0;
 	}
 	int evens = 0;
-	for (int numbs = 1; numbs <= i; numbs++)
+	for (int numbs = 1; numbs < i; numbs++)
 	{
-		while (i % numbs = 0)
+		if (i % numbs == 0)
 		{
 			evens += 1;
 		}
 	}
 	return evens;
+}
