f19a6fb4b2ff60fed1d6fe51ba6c6e3ee9872d85
Alex Frania <alexfrania96@gmail.com>
2019-09-25 21:24:41 -0800

    Bool function made.

--- HW/HW2/names.cpp
+++ HW/HW2/names.cpp
@@ -16,6 +16,19 @@ void InputNames(std::vector<std::string>& names)
 	names.push_back(x);
 }
 
+bool DoesNameExist(const std::string& nametoFind, const std::vector<std::string>& names)
+{
+	for (int i = 0; i < names.size(); i++)
+	{
+		if (names[i] == nametoFind)
+		{
+			return true;
+			break;
+		}
+	}
+	return false;
+}
+
 int main(int argc, char **argv)
 {
 	std::vector <std::string> names;
