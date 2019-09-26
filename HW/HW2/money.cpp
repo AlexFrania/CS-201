30f0476fe397e2249dc3f936487911fb57aefd4e
Alex Frania <alexfrania96@gmail.com>
2019-09-25 21:55:32 -0800

    Made a total double to add in all the coins. Also asked user to input coins.

--- HW/HW2/money.cpp
+++ HW/HW2/money.cpp
@@ -10,5 +10,17 @@ int main()
 	double dimes = 0;
 	double quarters = 0;
 
+	double total;
+
+	cout << "How many pennies do you have?: ";
+	cin >> pennies;
+	cout << "How many nickles?: ";
+	cin >> nickles;
+	cout << "How many dimes?: ";
+	cin >> dimes;
+	cout << "How many quarters?: ";
+	cin >> quarters;
+	cout << "\n";
+
 	return 0;
 }
