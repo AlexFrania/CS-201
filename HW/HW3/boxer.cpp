25ba35d38591dd7df8d9b11d1c76377ba306a91a
Alex Frania <alexfrania96@gmail.com>
2019-09-27 01:18:14 -0800

    Finished boxer.cpp

--- HW/HW3/boxer.cpp
+++ HW/HW3/boxer.cpp
@@ -2,10 +2,48 @@
 #include "boxerprint.h"
 #include <string>
 
+using std::cout;
+using std::cin;
+
 int main()
 {
-	std::string input;
-	std::cout << "Enter anything: ";
+	std::string boxed;
+	int boxer;
+	bool proof = false;
+	bool quitter = false;
 
+	cout << "Enter in " << '"' << "stop" << '"' << " when asked to input words to terminate program.\n\n";
+
+	while (quitter == false)
+	{
+		cout << "Enter in a word, phrase, or character: ";
+		std::getline(cin, boxed);
+
+		if (boxed == "stop")
+		{
+			cout << "Fine, have it your way!\n";
+			break;
+		}
+		cout << "Now enter in an integer: ";
+		cin.clear();
+		cin >> boxer;
+
+		while (proof == false)
+		{
+			if (boxer <= 0)
+			{
+				cout << "Please enter in a positive integer: ";
+				cin >> boxer;
+			}
+			else
+			{
+				proof = true;
+			}
+		}
+		cout << "\n";
+
+		BoxerPrint(boxed, boxer);
+		cin.ignore();
+	}
 	return 0;
 }
--- HW/HW3/boxerprint.cpp
+++ HW/HW3/boxerprint.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 #include "boxerprint.h"
 
+using std::cout;
+
 void BoxerPrint(std::string input, int n)
 {
+	int dub = n * 2;
 
+	for (int i = 1; i <= n; i++)
+	{
+		for (int x = 1; x <= input.size() + dub + 2; x++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 1; i <= 3; i++)
+	{
+		for (int x = 1; x <= n; x++)
+		{
+			cout << "*";
+		}
+			
+		if (i == 1 || i == 3)
+		{
+			for (int a = 1; a <= input.size() + 2; a++)
+			{
+				cout << " ";
+			}
+		}
+		else
+		{
+			cout << " " << input << " ";
+		}
+
+		for (int x = 1; x <= n; x++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int x = 1; x <= input.size() + dub + 2; x++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
 }
