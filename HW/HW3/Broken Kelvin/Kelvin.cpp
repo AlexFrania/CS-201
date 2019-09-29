3b2f671e63101a78be8748eb5e1501966b346061
Alex Frania <alexfrania96@gmail.com>
2019-09-28 19:26:13 -0800

    Fixed Kelvin.cpp and made notes on broken code.

---
+++ HW/HW3/Broken Kelvin/Kelvin.cpp
@@ -0,0 +1,46 @@
+/* double ctok(double c)
+{
+	int k = c + 273.15; // k should be a double, not an integer.
+	return int; // You can't return an integer like this. Return k.
+}
+
+int main()
+{
+	double c = 0; 
+	cin >> d; // d should be c. Cin should be std::cin
+	double k = ctok("c") // "c" is not a data type that is taken in by the function ctok(). Also the code should end with ;
+	cout << k << '/n'; // cout should be std::cout
+}*/
+
+#include <iostream>
+#include <iomanip>
+
+double ctok(double c)
+{
+	bool proof = false;
+
+	while (proof == false)
+	{
+		if (c < -273.15)
+		{
+			std::cout << "\nERROR: Value needs to be greater than or equal to -273.15\n";
+			std::cout << "Enter another value: ";
+			std::cin >> c;
+		}
+		else
+		{
+			proof = true;
+		}
+	}
+	double k = c + 273.15;
+	return k;
+}
+
+int main()
+{
+	std::cout << "Please enter a temperature in Celsius greater than or equal to -273.15: ";
+	double c = 0;
+	std::cin >> c;
+	double k = ctok(c);
+	std::cout << "\nYour temperature is " << k << " Kelvin.\n";
+}
