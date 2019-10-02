aad1b1d884590fbda1f3592f23e5c1256042d52e
Alex Frania <alexfrania96@gmail.com>
2019-10-01 19:36:21 -0800

    Finished names.cpp

--- HW/HW2/names.cpp
+++ HW/HW2/names.cpp
@@ -2,16 +2,55 @@
 CS-201
 Date: 9/18/19
 HW2: Names
-Description: 
+Description: Program takes in user input into a vector. Once in the vector, the program can
+find whether a name that the the user inputs is already in the vector array or not. The program
+then prints the names from the vector back out. Finally, the program lists the length of characters
+for each name in the order the names were originally input.
 */
+
 #include <iostream>
 #include <vector>
 #include <string>
 #include <algorithm>
 
+bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names)
+{
+	for (int i = 0; i < names.size(); i++)
+	{
+		if (names[i] == nameToFind)
+		{
+			std::cout << "This name IS in the list.\n";
+			return true;
+			break;
+		}
+	}
+	std::cout << "This name is NOT in the list.\n";
+	return false;
+}
+
+void PrintNames(const std::vector<std::string>& names)
+{
+	std::cout << "\nPrint Names\n";
+	for (int i = 0; i < names.size(); i++)
+	{
+		std::cout << names[i];
+		std::cout << "\n";
+	}
+}
+
+void PrintChar(const std::vector<std::string>& names)
+{
+	std::cout << "\nPrint Character Length\n";
+	for (int i = 0; i < names.size(); i++)
+	{
+		std::cout << names[i].size();
+		std::cout << "\n";
+	}
+}
+
 void InputNames(std::vector<std::string>& names)
 {	
-	for (int i = 0; i < 10; i++)
+	for (int i = 1; i <= 10; i++)
 	{
 		std::string name;
 
@@ -23,32 +62,13 @@ void InputNames(std::vector<std::string>& names)
 		{
 			std::string nameToFind;
 
-			std::cout << "What name would you like to look "
-			DoesNameExist(nameToFind, names); // Working on this part
+			std::cout << "\nSearch for name in list: ";
+			std::cin >> nameToFind;
+			DoesNameExist(nameToFind, names);
+			PrintNames(names);
+			PrintChar(names);
 		}
 	}	
-}
-
-bool DoesNameExist(const std::string& nameToFind, const std::vector<std::string>& names)
-{
-	for (int i = 0; i < names.size(); i++)
-	{
-		if (names[i] == nameToFind)
-		{
-			return true;
-			break;
-		}
-	}
-	return false;
-}
-
-void PrintNames(const std::vector<std::string>& names)
-{
-	for (int i = 0; i < names.size(); i++)
-	{
-		std::cout << names[i];
-		std::cout << "\n";
-	}
 }
 
 int main(int argc, char **argv)
